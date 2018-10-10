02535b3bc7de7789f894c6b2c52883a533d49cb6
atfcreative <atfcreative@gmail.com>
2018-10-10 11:51:06 -0700

    updated mongo connection in index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+mongoose.connect( process.env.MONGODB_URI || "Connected on 27017..." );
 
 const UserSchema = new Schema;
-	
+
