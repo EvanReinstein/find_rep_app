f78aadece5f8075ade099322f9315bd1da6949d9
EvanReinstein <edreins@gmail.com>
2018-10-13 11:44:29 -0700

    adds card index in order to target representative

--- public/scripts/results.js
+++ public/scripts/results.js
@@ -15,7 +15,7 @@ window.onload = function() {
 		//clears out HTML that contains representative cards
 		repCards.empty();
 		//create HTML code to be appended to representative cards
-		let repCardsHtml = reps.map( (rep) => {
+		let repCardsHtml = reps.map( (rep, i) => {
             //If line 2 doesn't exist, display empty string
             let line2 = rep.address[0].line2 === undefined ? '' : `${rep.address[0].line2} `
             //If part affiliation doesn't exist, display empty string
@@ -23,11 +23,11 @@ window.onload = function() {
             return `
 			<div class="card mb-4 shadow-sm">
               <div class="image-cropper">
-              <img id="photo" class="card-img-top smaller clip-circle" src="${rep.photoUrl[0]}" alt="Card image cap">
+              <img id="photo${i}" class="card-img-top smaller clip-circle" src="${rep.photoUrl[0]}" alt="Card image cap">
               </div>
               <div class="card-body">
-                  <h3 id="repName"class="card-text">${rep.name}</h3>
-                  <p id="party">Party Affiliation: ${ party }</p>
+                  <h3 id="repName${i}"class="card-text">${rep.name}</h3>
+                  <p id="party${i}">Party Affiliation: ${ party }</p>
                   <p>${rep.address[0].line1} ${ line2 }${rep.address[0].city}, ${rep.address[0].state}, ${rep.address[0].zip}</p>
                   <p>${rep.phones[0]}</p>
                   <p>${rep.urls[0]}</p>
@@ -39,16 +39,16 @@ window.onload = function() {
                     </div>
                     <div class="form-profile hidden">
                         <h1>Check Your Rep- Send a letter...</h1>
-                        <form id="messageData">
-                            <input id="messageName" type="text" name="field1" placeholder="Your Name" />
-                            <input id="messageEmail" type="email" name="field2" placeholder="Email Address" />
-                            <textarea  id="messageText" name="field3" placeholder="Dear Representative..."></textarea>
-                            <input type="submit" value="Send Letter" id="saveMessage" />
+                        <form id="messageData${i}">
+                            <input id="messageName${i}" type="text" name="field1" placeholder="Your Name" />
+                            <input id="messageEmail${i}" type="email" name="field2" placeholder="Email Address" />
+                            <textarea  id="messageText${i}" name="field3" placeholder="Dear Representative..."></textarea>
+                            <input type="submit" value="Send Letter" id="saveMessage" data-id="${i}" />
                         </form>
                     </div>
                     <div class="d-flex justify-content-between align-items-center">
 	                    <div class="btn-group">
-	                      <button type="button" class="btn btn-sm btn-outline-secondary" data-id="${rep._id}">Message</button>
+	                      <button type="button" class="btn btn-sm btn-outline-secondary" data="${rep._id}">Message</button>
 	                    </div>
                     </div>
               </div>
@@ -78,17 +78,19 @@ window.onload = function() {
 const baseURL = '/api/saved-messages';
 
 document.addEventListener('click',function(e) {
+
+
     if(e.target && e.target.id === 'saveMessage') {
         e.preventDefault();
-
+				let repNum = e.target.getAttribute('data-id');
         //Ids: messageName, messageText, messageEmail, messageData
 				// const repName = document.getElementById('messageData').getAttribute('data-id');
-				const photo = document.getElementById('photo').getAttribute('src');
-				const repName = document.getElementById('repName').innerText;
-				const party = document.getElementById('party').innerText;
-        const messageName = document.getElementById('messageName').value;
-        const messageEmail = document.getElementById('messageEmail').value;
-        const messageText = document.getElementById('messageText').value;
+				const photo = document.getElementById(`photo${repNum}`).getAttribute('src');
+				const repName = document.getElementById(`repName${repNum}`).innerText;
+				const party = document.getElementById(`party${repNum}`).innerText;
+        const messageName = document.getElementById(`messageName${repNum}`).value;
+        const messageEmail = document.getElementById(`messageEmail${repNum}`).value;
+        const messageText = document.getElementById(`messageText${repNum}`).value;
         const data = {photoUrl: photo, representative: repName, partyAffiliation: party, name: messageName, email: messageEmail, content: messageText};
         console.log(data);
 
